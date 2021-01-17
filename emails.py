34579701ba8a31fd43eecb269c190ee6bb30fd04
VarunSharma1216 <74435849+VarunSharma1216@users.noreply.github.com>
2021-01-17 10:52:34 -0800

    Add files via upload

---
+++ emails.py
@@ -0,0 +1,19 @@
+import smtplib
+smtp_object = smtplib.SMTP('smtp.gmail.com',587)
+smtp_object.ehlo()
+smtp_object.starttls()
+import getpass
+email = getpass.getpass('Email: ')
+password = getpass.getpass('Password: ')
+smtp_object.login(email,password)
+from_address = email
+to_address = input("Enter the emails of whoever you want to send it to: ").split()
+def send_email():
+    subject = input('Enter the subject line: ')
+    name = input("Who are you sending the email to: ")
+    body = input("What is the message you want to send: " )
+    your_name = input("Enter your full name: ")
+    message = "Hello " + name+ ","+ "\n" +body +"\n"+"Thanks,"+"\n"+your_name
+    msg = 'Subject: '+subject+'\n'+message
+    smtp_object.sendmail(from_address,to_address,msg)
+send_email()
